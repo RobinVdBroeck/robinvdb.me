6980483810a48719a2b3ea6f4ec7b65333a5eb19
Robin Van den Broeck <robin.broeck@gmail.com>
2018-04-29 12:18:21 +0200

    Added basic project

--- src/Containers/projects.js
+++ src/Containers/projects.js
@@ -2,6 +2,13 @@ import React from "react";
 
 export default class extends React.Component {
   render() {
-    return <div>Projects</div>;
+    return (
+      <div>
+        <p>
+          Mijn projecten zijn te vinden op{" "}
+          <a href="https://www.github.com/gamesmaxed">Github</a>
+        </p>
+      </div>
+    );
   }
 }
