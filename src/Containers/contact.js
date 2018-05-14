df658b956f06efa13d210bfe24013fc341773533
Robin Van den Broeck <robin.broeck@gmail.com>
2018-05-14 15:50:37 +0200

    Made it possible to send a real email

--- src/Containers/contact.js
+++ src/Containers/contact.js
@@ -5,72 +5,94 @@ export default class extends React.Component {
   render() {
     return (
       <div>
-        <div className="field is-horizontal">
-          <div className="field-label">
-            <label className="label">From:</label>
-          </div>
-          <div className="field-body">
-            <div className="field">
-              <div className="control has-icons-left">
-                <input className="input" type="text" placeholder="Name" />
-                <span className="icon is-small is-left">
-                  <FontAwesomeIcon icon={["fas", "user"]} />
-                </span>
+        <form
+          method="post"
+          action="https://formspree.io/robin.vandenbroeck@student.ucll.be"
+        >
+          <input type="hidden" name="_language" value="nl" />
+          <div className="field is-horizontal">
+            <div className="field-label">
+              <label className="label">Contactgegevens:</label>
+            </div>
+            <div className="field-body">
+              <div className="field">
+                <div className="control has-icons-left">
+                  <input
+                    className="input"
+                    type="text"
+                    placeholder="Robin Van den Broeck"
+                    name="name"
+                  />
+                  <span className="icon is-small is-left">
+                    <FontAwesomeIcon icon={["fas", "user"]} />
+                  </span>
+                </div>
+              </div>
+              <div className="field">
+                <div className="control has-icons-left">
+                  <input
+                    className="input"
+                    type="email"
+                    placeholder="jouw-email@jouw-bedrijf.be"
+                    name="_replyto"
+                  />
+                  <span className="icon is-small is-left">
+                    <FontAwesomeIcon icon={["fas", "envelope"]} />
+                  </span>
+                </div>
               </div>
             </div>
-            <div className="field">
-              <div className="control has-icons-left">
-                <input className="input" type="email" placeholder="Email" />
-                <span className="icon is-small is-left">
-                  <FontAwesomeIcon icon={["fas", "envelope"]} />
-                </span>
-              </div>
-            </div>
           </div>
-        </div>
 
-        <div className="field is-horizontal">
-          <div className="field-label" />
-          <div className="field-body">
-            <div className="field">
-              <div className="control has-icons-left">
-                <input
-                  className="input"
-                  type="tel"
-                  placeholder="Your phone number"
-                />
-                <span className="icon is-small is-left">
-                  <FontAwesomeIcon icon={["fas", "mobile"]} />
-                </span>
+          <div className="field is-horizontal">
+            <div className="field-label" />
+            <div className="field-body">
+              <div className="field">
+                <div className="control has-icons-left">
+                  <input
+                    className="input"
+                    type="tel"
+                    placeholder="+32 999 99 99 99"
+                    name="phone_number"
+                  />
+                  <span className="icon is-small is-left">
+                    <FontAwesomeIcon icon={["fas", "mobile"]} />
+                  </span>
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <div className="field is-horizontal">
-          <div className="field-label">
-            <label className="label">Message:</label>
+          <div className="field is-horizontal">
+            <div className="field-label">
+              <label className="label">Bericht:</label>
+            </div>
+            <div className="field-body">
+              <div className="field">
+                <div className="control">
+                  <textarea
+                    className="textarea"
+                    placeholder="Hoe kan ik je helpen"
+                    name="message"
+                  />
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="field-body">
-            <div className="field">
+
+          <div className="field is-horizontal">
+            <div className="field-label" />
+            <div className="field-body">
               <div className="control">
-                <textarea
-                  className="textarea"
-                  placeholder="Explain how I can help you"
+                <input
+                  type="submit"
+                  className="button is-primary"
+                  value="Verstuur email"
                 />
               </div>
             </div>
           </div>
-        </div>
-
-        <div className="field is-horizontal">
-          <div className="field-label" />
-          <div className="field-body">
-            <div className="control">
-              <button className="button is-primary">Submit</button>
-            </div>
-          </div>
-        </div>
+        </form>
       </div>
     );
   }
