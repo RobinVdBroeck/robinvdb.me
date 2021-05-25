4eadbf4b1705bbdf2ceb3c98d584ed74d2e9757c
Robin Van den Broeck <robin.broeck@gmail.com>
2021-05-25 23:39:02 +0200

    Use robinvdb.me as title everywhere

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Nav from "./nav";
+import Head from "next/head";
 
 export default function Layout({ children }) {
   return (
     <React.Fragment>
+      <Head>
+        <title>robinvdb.me</title>
+      </Head>
       <Nav />
       <div className="container">{children}</div>
     </React.Fragment>
